391894b91ecb6174c1848a9cda7bd71a30186157
sashok1987nik <sashok1987nk@ukr.net>
2024-01-05 17:05:59 +0100

    up

--- js/task-2.js
+++ js/task-2.js
@@ -2,6 +2,10 @@
 
 function calcAverageCalories(days) {
 
+    if (days.length === 0) {
+        return 0;
+      }
+
     let totalCalories = 0;
     
     for (let day of days) {
@@ -21,3 +25,19 @@ console.log(
       { day: "sunday", calories: 3300 }
     ])
   ); // 3180
+  
+  console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 2040 },
+      { day: "tuesday", calories: 2270 },
+      { day: "wednesday", calories: 2420 },
+      { day: "thursday", calories: 1900 },
+      { day: "friday", calories: 2370 },
+      { day: "saturday", calories: 2280 },
+      { day: "sunday", calories: 2610 }
+    ])
+  ); // 2270
+  
+  console.log(
+    calcAverageCalories([])
+  ); // 0
